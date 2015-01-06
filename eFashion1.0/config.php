8d8fabc75d5c2c3292643fb66863b161b9c1d9a4
MesiterWebzr <meister@fullsail.edu>
2015-01-06 10:27:08 -0800

    Added efashion1.0 dircetory

---
+++ eFashion1.0/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8889/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8889/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Meister/Documents/FS University/PPW/testSite/eFashion1.0/catalog/');
+define('DIR_SYSTEM', '/Users/Meister/Documents/FS University/PPW/testSite/eFashion1.0/system/');
+define('DIR_LANGUAGE', '/Users/Meister/Documents/FS University/PPW/testSite/eFashion1.0/catalog/language/');
+define('DIR_TEMPLATE', '/Users/Meister/Documents/FS University/PPW/testSite/eFashion1.0/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/Meister/Documents/FS University/PPW/testSite/eFashion1.0/system/config/');
+define('DIR_IMAGE', '/Users/Meister/Documents/FS University/PPW/testSite/eFashion1.0/image/');
+define('DIR_CACHE', '/Users/Meister/Documents/FS University/PPW/testSite/eFashion1.0/system/cache/');
+define('DIR_DOWNLOAD', '/Users/Meister/Documents/FS University/PPW/testSite/eFashion1.0/system/download/');
+define('DIR_UPLOAD', '/Users/Meister/Documents/FS University/PPW/testSite/eFashion1.0/system/upload/');
+define('DIR_MODIFICATION', '/Users/Meister/Documents/FS University/PPW/testSite/eFashion1.0/system/modification/');
+define('DIR_LOGS', '/Users/Meister/Documents/FS University/PPW/testSite/eFashion1.0/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'eFashion_user');
+define('DB_PASSWORD', '1723boss');
+define('DB_DATABASE', 'eFashion_db');
+define('DB_PREFIX', 'oc_');
