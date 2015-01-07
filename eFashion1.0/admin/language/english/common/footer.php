9cea6f363064a07f6d08b6b1c615d3236c7d2d93
MesiterWebzr <meister@fullsail.edu>
2015-01-07 02:11:23 -0800

    theme build

--- eFashion1.0/admin/language/english/common/footer.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Text
-$_['text_footer'] 	= '<a href="http://www.socalfreelancer.net">Meister</a> &copy; 2014-' . date('Y') . ' All Rights Reserved.';
-$_['text_version'] 	= 'Version %s';
--- eFashion1.0/admin/view/template/common/footer.tpl
+++
@@ -1,2 +0,0 @@
-<footer id="footer"><?php echo $text_powered; ?><br /><?php echo $text_version; ?></footer></div>
-</body></html>
